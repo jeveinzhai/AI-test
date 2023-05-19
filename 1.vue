e822cfd020ed7b11ec8a406d69e4270537a0ed26
jeveinzhai <jeveinzhai@gmail.com>
2023-05-19 19:19:22 +0800

    第一次全部提交

---
+++ 1.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      <circle-bubble v-for="(bubble, index) in bubbles" :key="index" :text="bubble.text" @click="showBubble(bubble)" />
+      <bubble-modal v-if="selectedBubble" :text="selectedBubble.text" @close="selectedBubble = null" />
+    </div>
+  </template>
+  
+  <script>
+  import CircleBubble from './CircleBubble.vue'
+  import BubbleModal from './BubbleModal.vue'
+  
+  export default {
+    components: {
+      CircleBubble,
+      BubbleModal
+    },
+    data() {
+      return {
+        bubbles: [
+          { text: '气泡1' },
+          { text: '气泡2' },
+          { text: '气泡3' }
+        ],
+        selectedBubble: null
+      }
+    },
+    methods: {
+      showBubble(bubble) {
+        this.selectedBubble = bubble
+      }
+    }
+  }
+  </script>
+  
